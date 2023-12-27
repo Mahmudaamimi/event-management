d7f2359395e087e85ce33c8570509ffc0927f709
unknown <mahmuda1mimi@gmail.com>
2023-12-27 18:26:11 +0600

    update

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -6,11 +6,11 @@ const Blog = () => {
         <div className="mt-8">
             <Navbar></Navbar>
             <div className="mb-6">
-                <img className="h-auto w-full" src="https://i.ibb.co/KVtpmZ5/edu-1.jpg" alt="" />
+                <img className="h-auto w-full" src="https://i.ibb.co/Jpwb407/6.jpg" alt="" />
                 <p className="mt-7 leading-10 text-gray-500">This is the structured form of education that takes place within an organized educational institution, following a specific curriculum. It usually involves classrooms, teachers, textbooks, examinations, and recognized certifications or degrees.Informal education happens outside formal settings and includes learning from daily experiences, interactions with family and friends, observing and imitating others, and engaging with the community. Informal education is equally valuable and contributes significantly to an individual's overall learning.</p>
             </div>
             <div className="mb-6">
-                <img className="h-auto w-full" src="https://i.ibb.co/z8TkSQN/edu-4.jpg" alt="" />
+                <img className="h-auto w-full" src="https://i.ibb.co/fr6PPgK/7.jpg" alt="" />
                 <p className="mt-7 leading-10 text-gray-500">Education stands as a powerful catalyst, capable of shaping societies and transforming individual lives. In this blog post, we'll delve into why education plays such a critical role and how it impacts our world.Education stands as a powerful catalyst, capable of shaping societies and transforming individual lives. In this blog post, we'll delve into why education plays such a critical role and how it impacts our world.</p>
             </div>
 
